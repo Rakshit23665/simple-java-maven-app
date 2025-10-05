439380c48a1b2799eeb48d22131b6978463673b6
RakshitSharma <2022.rakshit.sharma@ves.ac.in>
2025-10-05 20:14:33 +0530

    Update deliver.bat

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -1,24 +1,14 @@
 @echo off
 
-echo The following Maven command installs your Maven-built Java application
-echo into the local Maven repository, which will ultimately be stored in
-echo Jenkins's local Maven repository (and the "maven-repository" Docker data
-echo volume).
-
-REM Run Maven commands to build, install, and evaluate project properties
 call mvn jar:jar install:install help:evaluate -Dexpression=project.name
 
-echo The following command extracts the value of the ^<name/^> element
-echo within ^<project/^> of your Java/Maven project's "pom.xml" file.
+for /f "delims=" %%n in ('mvn -q -DforceStdout help:evaluate -Dexpression=project.name') do set NAME=%%n
+for /f "delims=" %%v in ('mvn -q -DforceStdout help:evaluate -Dexpression=project.version') do set VERSION=%%v
 
-for /f "usebackq delims=" %%n in (`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`) do set NAME=%%n
+echo Project name is: %NAME%
+echo Project version is: %VERSION%
 
-echo The following command behaves similarly to the previous one but
-echo extracts the value of the ^<version/^> element within ^<project/^> instead.
+dir target
 
-for /f "usebackq delims=" %%v in (`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`) do set VERSION=%%v
+java -jar target\%NAME%-%VERSION%.jar
 
-echo The following command runs and outputs the execution of your Java
-echo application (which Jenkins built using Maven) to the Jenkins UI.
-
-call java -jar target\%NAME%-%VERSION%.jar
